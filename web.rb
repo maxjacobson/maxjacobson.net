8e3f1d5eb5d617e6a2920fe6d0066b680f253759
Max Jacobson <maxwell.jacobson@gmail.com>
2013-06-09 16:00:19 -0400

    added feed redirect

--- web.rb
+++ web.rb
@@ -3,6 +3,11 @@ require 'sinatra'
 get '/' do
   redirect "http://maxjacobson.github.io"
 end
+
+get '/feed' do
+  redirect "http://maxjacobson.github.io/atom.xml"
+end
+
 get '/:anything' do
   redirect "http://maxjacobson.github.io"
 end
