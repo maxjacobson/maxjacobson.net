b46832c72286d07928efd86aa9d942460a28dcaa
Max Jacobson <max@hardscrabble.net>
2014-11-28 17:13:50 -0500

    update redirects to latest correct destinations

--- web.rb
+++ web.rb
@@ -1,21 +1,22 @@
 require 'sinatra'
 
 get '/' do
-  redirect "http://maxjacobson.github.io"
+  redirect "http://www.hardscrabble.net"
 end
 
 get '/feed' do
-  redirect "http://maxjacobson.github.io/atom.xml"
+  redirect "http://www.hardscrabble.net/atom.xml"
 end
 
 get '/~about' do
-  redirect "http://maxjacobson.github.io/blog/about"
+  redirect "http://www.hardscrabble.net/about"
 end
 
 get '/~projects' do
-  redirect "http://maxjacobson.github.io/blog/about"
+  redirect "http://www.hardscrabble.net"
 end
 
 get '/:anything' do
-  redirect "http://maxjacobson.github.io"
+  redirect "http://www.hardscrabble.net"
 end
+
